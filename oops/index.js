4f572ecac50a8c723bbcbaea0eed5963b9e6c8dc
raghav bhardwaj <braghav474@gmail.com>
2026-02-24 14:42:23 +0530

    oop intro

---
+++ oops/index.js
@@ -0,0 +1,56 @@
+// oop => object oriented programming (just a programming style)
+
+// object - collection of properties and methods
+
+// components of oop
+     /*
+      - object literals
+      - constrtuctor function
+      - prototypes
+      - classes
+      - instances (new,this)
+     */
+
+
+      const user = {
+         name:"test",
+         age:21,
+         isLoggedIn:false,
+         sayHello : function () {
+            console.log(`hello ${this.name}`);
+            // this => current context
+            // console.log(this);
+            
+         }
+      }
+
+    //   console.log(user.name);
+    //   console.log(user.sayHello());
+    //   console.log(this);
+      
+
+    //   what if we want to make a user 2,3,4 and so on
+    //    ----> constructor functions
+
+    //   const promiseOne = new Promise()
+    // new keyword = constructor function
+
+
+    function User (username,age,isLoggedIn) {
+           this.username = username
+           this.age = age
+           this.isLoggedIn = isLoggedIn
+           this.sayHello = function () {
+               console.log(`welcome ${this.username}`);
+               
+           }
+
+        //    return this
+    }  
+
+
+    const userOne = new User("raghav",21,true)
+     const userTwo = new User("another user",25,false)
+    console.log(userOne.sayHello());
+    
+    
